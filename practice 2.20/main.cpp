0dae9b3cd2d7076636bdd1a6ac633fd699b2334c
junklasagna <redlinetango@gmail.com>
2022-03-18 17:12:31 +0800

    file uploaded

---
+++ practice 2.20/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float r; //radius
+  cout << "Input radius and press enter " << endl;
+  cin >> r;
+
+  cout << "Circle's diameter is " << r * 2 << endl;
+
+  cout << "Circles's circumference is " << r * 2 * 3.14159 << endl;
+
+  cout << "Circle's area is " << r * r * 3.14159 << endl;
+
+      return 0;
+}
